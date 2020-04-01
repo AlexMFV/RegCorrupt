80a6237d733cafd6e0eb7349437d04dedae1d4c1
AlexMFV <alexmfv1@gmail.com>
2020-04-01 12:51:36 +0100

    Project Init

---
+++ RegCorrupt/Form1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RegCorrupt
+{
+    public partial class Form1 : Form
+    {
+        RegistryKey def_path = Registry.LocalMachine;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
