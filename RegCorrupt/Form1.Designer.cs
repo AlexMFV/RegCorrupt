c1ff1345cefc4bf6a25bdb2eab0bf33db0d77f4f
AlexMFV <alexmfv1@gmail.com>
2020-04-02 13:42:37 +0100

    Added manifest, get full registry

--- RegCorrupt/Form1.Designer.cs
+++ RegCorrupt/Form1.Designer.cs
@@ -28,13 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lstRegistry = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
+            // 
+            // lstRegistry
+            // 
+            this.lstRegistry.FormattingEnabled = true;
+            this.lstRegistry.Location = new System.Drawing.Point(12, 12);
+            this.lstRegistry.Name = "lstRegistry";
+            this.lstRegistry.Size = new System.Drawing.Size(782, 433);
+            this.lstRegistry.TabIndex = 0;
+            this.lstRegistry.DoubleClick += new System.EventHandler(this.lstRegistry_DoubleClick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(806, 459);
+            this.Controls.Add(this.lstRegistry);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -42,6 +53,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lstRegistry;
     }
 }
 
--- RegCorrupt/Form1.cs
+++ RegCorrupt/Form1.cs
@@ -18,6 +18,34 @@ namespace RegCorrupt
         public Form1()
         {
             InitializeComponent();
+            GetFullRegistry();
+        }
+
+        public void GetFullRegistry()
+        {
+            string[] SubKeys = def_path.GetSubKeyNames();
+
+            foreach (string key in SubKeys)
+            {
+                if(key != "SECURITY")
+                    lstRegistry.Items.Add(key);
+            }
+        }
+
+        public void LoadSubKeys(RegistryKey path)
+        {
+            lstRegistry.Items.Clear();
+
+            foreach(string key in path.GetSubKeyNames())
+            {
+                lstRegistry.Items.Add(key);
+            }
+        }
+
+        private void lstRegistry_DoubleClick(object sender, EventArgs e)
+        {
+            if (lstRegistry.SelectedIndex != -1)
+                LoadSubKeys(def_path.OpenSubKey(lstRegistry.SelectedItem.ToString()));
         }
     }
 }
